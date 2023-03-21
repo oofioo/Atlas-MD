0a5ca61453b75d2207b864071866a880d778c745
oofioo <102270626+oofioo@users.noreply.github.com>
2023-03-21 11:07:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,16 +32,16 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "6591494480, 6588026916";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
-global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.mongodb = process.env.MONGODB || "mongodb+srv://oofio:oofio9118@atlas0.szunbql.mongodb.net/?retryWrites=true&w=majority";
+global.sessionId = process.env.SESSION_ID || "oofiooofiooofiooofiooofiooofio9118";
+global.prefa = process.env.PREFIX || ">";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas MD`;
